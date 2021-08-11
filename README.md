8213c3b961880ee4e3c7247eaadcd6a977304ce8
Nicolas Rebagliati <nrebagliati@faradaysec.com>
2021-08-11 14:28:32 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# py-sneakers
+
+
+### Install
+```
+git clone https://github.com/aenima-x/py-sneakers
+cd py-sneakers
+pip install .
+```
+
+### Usage:
+
+#### As a command:
+```
+echo "Text" |py-sneakers
+```
+
+#### As a lib:
+```python
+from py_sneakers import Sneakers
+
+Sneakers("Some text").decrypt()
+```
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+setup(
+    name="py-sneakers",
+    version="1.0",
+    url="https://github.com/aenima-x/py-sneakers",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    license="GNU General Public License v3",
+    author="Aenima-x",
+    python_requires=">3.7.0",
+    author_email="nicolas.rebagliati@aenima-x.com.ar",
+    description="Python library to emulate the Sneakers effect",
+    include_package_data=True,
+    entry_points={
+        "console_scripts": ["py-sneakers=py_sneakers:main"],
+    },
+)
