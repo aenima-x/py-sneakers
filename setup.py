2db64b129cd5f9b888282d6d59a2376464e67bd5
Nicolas Rebagliati <nrebagliati@faradaysec.com>
2021-08-12 15:22:57 -0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,14 @@
 from setuptools import setup
+
 setup(
-    name="py-sneakers",
-    version="1.0",
-    url="https://github.com/aenima-x/py-sneakers",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-    ],
-    license="MIT",
-    author="Aenima-x",
-    python_requires=">3.7.0",
-    author_email="nicolas.rebagliati@aenima-x.com.ar",
-    description="Python library to emulate the Sneakers effect",
-    include_package_data=True,
+    name='py-sneakers',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/aenima-x/py-sneakers',
+    license='MIT',
+    author='Nicolas Rebagliati',
+    author_email='nicolas.rebagliati@aenima-x.com.ar',
+    description='Python library to emulate the Sneakers effect',
     entry_points={
         "console_scripts": ["py-sneakers=py_sneakers:main"],
     },
